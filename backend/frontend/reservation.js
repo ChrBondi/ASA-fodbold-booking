a4a1c1de6c784dbd2172dba976a40bc15b444836
Leaq <frederikgrummesgaard@gmail.com>
2018-11-28 11:34:56 +0100

    Reservation classe er oprettet

---
+++ backend/frontend/reservation.js
@@ -0,0 +1,18 @@
+class reservation {
+
+    date;
+    footballField;
+    start;
+    end;
+    lights;
+    lockerRoom;
+    renter;
+    contactPerson;
+    mail;
+    phone;
+    price;
+    comments;
+}
+
+
+const priceList = {kunst11m : 650, kunst8m : 325, kunst5m : 200, kunst3m : 150, kunstFutsall : 150}
