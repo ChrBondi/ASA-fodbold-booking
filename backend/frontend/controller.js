85668bb90302f19dd7e383bd67d33759062dae04
Anna <annakrogh13@hotmail.com>
2018-11-30 13:29:51 +0100

    et eller andet

--- backend/frontend/controller.js
+++ backend/frontend/controller.js
@@ -6,46 +6,28 @@ onload = async () => {
     compiledBookingPerDate = Handlebars.compile(templateText);
     console.log(compiledBookingPerDate);
 
-    // const name = document.querySelector('#name');
-    // const password = document.querySelector('#password');
-    // const button = document.querySelector('#button');
-    // const error = document.querySelector('#error');
-    //
-    // button.onclick = async () => {
-    //     const data = {name: name.value, password: password.value};
-    //     const result = await fetch("/login", {
-    //         method: "POST",
-    //         body: JSON.stringify(data),
-    //         headers: {'Content-Type': 'application/json'}
-    //     });
-    //     const answer = await result.json();
-    //     if (answer.ok)
-    //         window.location.href = "/session";
-    //     else {
-    //         error.innerHTML = "Login fejl!";
-    //     }
-    // }
+    const button = document.querySelector('#button');
+    if(button){
+        button.onclick = login;
+    }
 };
 
-function login() {
+async function login() {
     const name = document.querySelector('#name');
     const password = document.querySelector('#password');
-    const button = document.querySelector('#button');
     const error = document.querySelector('#error');
+    const data = {name: name.value, password: password.value};
 
-    button.onclick = async () => {
-        const data = {name: name.value, password: password.value};
-        const result = await fetch("/login", {
-            method: "POST",
-            body: JSON.stringify(data),
-            headers: {'Content-Type': 'application/json'}
-        });
-        const answer = await result.json();
-        if (answer.ok)
-            window.location.href = "/session";
-        else {
-            error.innerHTML = "Login fejl!";
-        }
+    const result = await fetch("/login", {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {'Content-Type': 'application/json'}
+    });
+    const answer = await result.json();
+    if (answer.ok)
+        window.location.href = "/session";
+    else {
+        error.innerHTML = "Login fejl!";
     }
 };
 
